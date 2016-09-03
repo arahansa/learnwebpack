adb79f0010eba3f93012d2fcb3deba5e9f5917a7
jarvis <arahansa@naver.com>
2016-09-03 21:38:01 +0900

    first web pack

---
+++ entry.js
@@ -0,0 +1,5 @@
+require("./node_modules/jquery/dist/jquery.min.js")
+require("bootstrap-webpack");
+/*require("./node_modules/bootstrap/dist/css/bootstrap.min.css")*/
+require("./node_modules/font-awesome/css/font-awesome.min.css")
+/*require("./node_modules/bootstrap/dist/js/bootstrap.min.js")*/
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url'
+            },
+            { test: /\.css$/, loader: 'style!css'}    
+        ]
+    }
+};
